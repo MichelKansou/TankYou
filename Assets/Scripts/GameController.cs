59353e38b52ea581eed7c0de231ccef2cd8a454f
MichelKansou <michel.kansou@outlook.fr>
2016-06-07 15:19:35 +0200

    Add Main Menu & Resume Menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject ResumeMenu;
+
+	// Use this for initialization
+	void Start () {
+		ResumeMenu.gameObject.SetActive(false);
+	}
+
+	public void ResumeGame(){
+		ResumeMenu.gameObject.SetActive(false);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (ResumeMenu.activeSelf == false && Input.GetKeyDown (KeyCode.Escape)) {
+			ResumeMenu.gameObject.SetActive (true);
+		} else if (ResumeMenu.activeSelf == true && Input.GetKeyDown (KeyCode.Escape)) {
+			ResumeMenu.gameObject.SetActive (false);
+		}
+	}
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+	public void LoadScene ( string name ){
+		SceneManager.LoadScene( name );
+		//Application.LoadLevel( name );
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void QuitGame(){
+		Application.Quit();
 	}
 }
